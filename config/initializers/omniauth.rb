d169df62bdf7d1b508b8a82a82f80e333a326719
paolotax <paolo.tassinari@gmail.com>
2012-02-28 08:00:35 +0100

    auth facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '5wQobM1Y0oSXalOEBYZPEg', 'bfacz8JzWuuZLJQX88SZ5dGusaPX1t7Wy36p2Z35dc'
-  provider :google_oauth2, ENV['AIzaSyCeNurhQKIJWAGS0rtubtq6jEiNM8HJ0U8'], ENV['gBz-bgbhC03I0laxGWthVC1E'], {access_type: 'online', approval_prompt: ''}
-  provider :facebook, ENV['182463618530472'], ENV['c2df69a3b6716d146972684051e4a996']
+  provider :google_oauth2, 'AIzaSyCeNurhQKIJWAGS0rtubtq6jEiNM8HJ0U8', 'gBz-bgbhC03I0laxGWthVC1E', {access_type: 'online', approval_prompt: ''}
+  provider :facebook, '182463618530472', 'c2df69a3b6716d146972684051e4a996'
 end
