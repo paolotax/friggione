6c0b9f2b7bc6faaec4c362a9541abbf0f7a19823
paolotax <paolo.tassinari@gmail.com>
2012-04-30 20:54:24 +0200

    migration

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ActiveRecord::Base
-
+  
+  belongs_to :user
+  
   has_many :ingredients,  dependent: :destroy
   has_many :instructions, dependent: :destroy
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120226065930) do
+ActiveRecord::Schema.define(:version => 20120301080331) do
 
   create_table "friendly_id_slugs", :force => true do |t|
-    t.string   "slug",                         :null => false
-    t.integer  "sluggable_id",                 :null => false
-    t.string   "sluggable_type", :limit => 40
-    t.datetime "created_at"
+    t.string    "slug",                         :null => false
+    t.integer   "sluggable_id",                 :null => false
+    t.string    "sluggable_type", :limit => 40
+    t.timestamp "created_at"
   end
 
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
@@ -25,57 +25,59 @@ ActiveRecord::Schema.define(:version => 20120226065930) do
   add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "ingredients", :force => true do |t|
-    t.string   "content"
-    t.string   "um"
-    t.string   "value"
-    t.integer  "position"
-    t.integer  "recipe_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string    "content"
+    t.string    "um"
+    t.string    "value"
+    t.integer   "position"
+    t.integer   "recipe_id"
+    t.timestamp "created_at", :null => false
+    t.timestamp "updated_at", :null => false
   end
 
   add_index "ingredients", ["recipe_id"], :name => "index_ingredients_on_recipe_id"
 
   create_table "instructions", :force => true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.string   "position"
-    t.string   "prep_time"
-    t.string   "photo"
-    t.integer  "recipe_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string    "title"
+    t.text      "content"
+    t.string    "position"
+    t.string    "prep_time"
+    t.string    "photo"
+    t.integer   "recipe_id"
+    t.timestamp "created_at", :null => false
+    t.timestamp "updated_at", :null => false
   end
 
   add_index "instructions", ["recipe_id"], :name => "index_instructions_on_recipe_id"
 
   create_table "recipes", :force => true do |t|
-    t.string   "title"
-    t.text     "summary"
-    t.string   "yield"
-    t.string   "duration"
-    t.string   "author"
-    t.string   "photo"
-    t.string   "video"
-    t.date     "published_at"
-    t.string   "cooking_time"
-    t.string   "prep_time"
-    t.string   "slug"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.string    "title"
+    t.text      "summary"
+    t.string    "yield"
+    t.string    "duration"
+    t.string    "author"
+    t.string    "photo"
+    t.string    "video"
+    t.date      "published_at"
+    t.string    "cooking_time"
+    t.string    "prep_time"
+    t.string    "slug"
+    t.timestamp "created_at",   :null => false
+    t.timestamp "updated_at",   :null => false
+    t.integer   "user_id"
   end
 
   add_index "recipes", ["slug"], :name => "index_recipes_on_slug"
+  add_index "recipes", ["user_id"], :name => "index_recipes_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "nickname"
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string   "image"
+    t.string    "provider"
+    t.string    "uid"
+    t.string    "name"
+    t.string    "nickname"
+    t.string    "email"
+    t.timestamp "created_at", :null => false
+    t.timestamp "updated_at", :null => false
+    t.string    "image"
   end
 
 end
